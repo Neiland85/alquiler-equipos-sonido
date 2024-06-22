c105cb74c9cad46bb5e96e117d61f3a1753c9597
Neil Muñoz Lago <164719485+Neiland85@users.noreply.github.com>
2024-06-22 23:51:39 +0200

    Update preload.php
    Update preload.php to handle both production and development environments

--- config/preload.php
+++ config/preload.php
@@ -2,4 +2,8 @@
 
 if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
     require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
+} elseif (file_exists(dirname(__DIR__).'/var/cache/dev/App_KernelDevContainer.preload.php')) {
+    require dirname(__DIR__).'/var/cache/dev/App_KernelDevContainer.preload.php';
+} else {
+    error_log('Preload file not found. Ensure the cache is properly generated.');
 }
