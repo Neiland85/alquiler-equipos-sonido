ce57a2b3aa9438c286042ba4ab9ec2bb052ad66f
Neil Muñoz Lago <164719485+Neiland85@users.noreply.github.com>
2024-06-22 23:38:27 +0200

    Update installed.php
    Update installed.php with the latest dependencies versions

--- assets/vendor/installed.php
+++ assets/vendor/installed.php
@@ -1,22 +1,146 @@
-<?php return array (
-  '@hotwired/stimulus' => 
+<?php
+
+// installed.php
+
+return array (
+  'root' => 
   array (
-    'version' => '3.2.2',
-    'dependencies' => 
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
     array (
     ),
-    'extraFiles' => 
-    array (
-    ),
+    'reference' => 'e50f8d0de9d71ed6b92702d7e93bc85743d726c5',
+    'name' => '__root__',
   ),
-  '@hotwired/turbo' => 
+  'versions' => 
   array (
-    'version' => '7.3.0',
-    'dependencies' => 
+    '__root__' => 
     array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e50f8d0de9d71ed6b92702d7e93bc85743d726c5',
     ),
-    'extraFiles' => 
+    'symfony/console' => 
     array (
+      'pretty_version' => 'v5.3.7',
+      'version' => '5.3.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5a22258e97e644ab7685276f74dc27bb20245891',
+    ),
+    'symfony/dependency-injection' => 
+    array (
+      'pretty_version' => 'v5.3.4',
+      'version' => '5.3.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2b398cae3e55de2ff6e7c5ae6b1f08dd4f6847ad',
+    ),
+    'symfony/error-handler' => 
+    array (
+      'pretty_version' => 'v5.3.4',
+      'version' => '5.3.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7d0e12507cc2a7b51bc9880cfbcfa5488e5675e5',
+    ),
+    'symfony/event-dispatcher' => 
+    array (
+      'pretty_version' => 'v5.3.7',
+      'version' => '5.3.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4dc3f3167243c0b53f669c9b5640b36c4c8f579d',
+    ),
+    'symfony/filesystem' => 
+    array (
+      'pretty_version' => 'v5.3.4',
+      'version' => '5.3.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '548a498b95b040d4e0105f83d4a1070b278b1a2b',
+    ),
+    'symfony/finder' => 
+    array (
+      'pretty_version' => 'v5.3.7',
+      'version' => '5.3.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6cc8bd8d28ff3d870a20e3857a700032a05443d3',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '46cd95797e9df938fdd2b03693b5fca5e64b01ce',
+    ),
+    'symfony/polyfill-intl-grapheme' => 
+    array (
+      'pretty_version' => 'v1.23.1',
+      'version' => '1.23.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '16880ba9c5e6e6b8ee102aa98acb9b4bb3c63f37',
+    ),
+    'symfony/polyfill-intl-normalizer' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8590a5f561694770bdcd3c9b9856179e749e3e0d',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.23.1',
+      'version' => '1.23.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9174a3d57b48b5a6b375a74d5f21a5fcbced81b6',
+    ),
+    'symfony/service-contracts' => 
+    array (
+      'pretty_version' => 'v2.4.0',
+      'version' => '2.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f040a30e04b57fbcc9c6cbcf4dbaa96bd318b9bb',
+    ),
+    'symfony/string' => 
+    array (
+      'pretty_version' => 'v5.3.7',
+      'version' => '5.3.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6c74f04d23b89cf52fbf7db982fde6b1b1b787bc',
+    ),
+    'symfony/yaml' => 
+    array (
+      'pretty_version' => 'v5.3.6',
+      'version' => '5.3.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e2c4af7c2c9d0e1800cdfe6fdfb4f496efdf4a23',
     ),
   ),
 );
+
