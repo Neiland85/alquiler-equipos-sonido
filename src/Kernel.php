1418ca77ce7a4bb0901e7ec545d602bd3c1f6eb2
Neil Muñoz Lago <164719485+Neiland85@users.noreply.github.com>
2024-06-23 00:36:42 +0200

    Update Kernel.php
    Update Kernel.php with additional configuration methods for container and routes

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,9 +3,40 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function configureContainer(ContainerBuilder $containerBuilder, LoaderInterface $loader)
+    {
+        $loader->load($this->getProjectDir() . '/config/packages/*.{php,yaml}', 'glob');
+        $loader->load($this->getProjectDir() . '/config/services.yaml');
+        $loader->load($this->getProjectDir() . '/config/packages/' . $this->environment . '/*.{php,yaml}', 'glob');
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        $routes->import($this->getProjectDir() . '/config/routes/*.{php,yaml}', '/', 'glob');
+        $routes->import($this->getProjectDir() . '/config/routes/' . $this->environment . '/*.{php,yaml}', '/', 'glob');
+    }
+
+    public function getProjectDir(): string
+    {
+        return dirname(__DIR__);
+    }
+
+    public function getCacheDir(): string
+    {
+        return $this->getProjectDir() . '/var/cache/' . $this->environment;
+    }
+
+    public function getLogDir(): string
+    {
+        return $this->getProjectDir() . '/var/log';
+    }
 }
